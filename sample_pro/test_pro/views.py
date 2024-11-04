af105b0bb8227b68e828552ba5546f78fa70eb3e
astabhuj <kritikasengar56@gmail.com>
2024-11-04 12:29:07 +0530

    con1234: initial commmit

---
+++ sample_pro/test_pro/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+@csrf_exempt
+def additionView(r):
+    #incoming = r.POST
+    incoming = json.loads(r.body)
+    print(incoming)
+    out = int(incoming['x']) + int(incoming['y'])
+    data = {'result':out}
+    return JsonResponse(data)
+
